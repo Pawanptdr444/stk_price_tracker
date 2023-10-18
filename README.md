95939ea37db50e3e95484439d78027c31973a20b
Pawanptdr444 <136560004+Pawanptdr444@users.noreply.github.com>
2023-10-18 12:46:08 +0530

    stockProject

---
+++ README.md
@@ -0,0 +1,28 @@
+<h1 align="center">Welcome to Stock price tracker 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-0.1.0-blue.svg?cacheSeconds=2592000" />
+</p>
+
+## Install
+
+```sh
+npm install
+```
+
+## Usage
+
+```sh
+npm run start
+```
+
+## Screenshot
+1. Homepage
+![Homepage](https://github.com/Dhaval-Tank/stock-price-tracker/blob/main/screenshot/stock-price.PNG?raw=true)
+
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ src/Stock.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import Plot from 'react-plotly.js';
+
+class Stock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stockChartXValues: [],
+      stockChartYValues: [],
+      symbol:"FB"
+    }
+  }
+
+  componentDidMount() {
+    this.fetchStock();
+  }
+
+  fetchStock() {
+    const pointerToThis = this;
+    const API_KEY = '9OZJOS9GDC3EQPLJ';
+    let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${this.state.symbol}&outputsize=compact&apikey=${API_KEY}`;
+    let stockChartXValuesFunction = [];
+    let stockChartYValuesFunction = [];
+
+    fetch(API_Call)
+      .then(
+        function(response) {
+          return response.json();
+        }
+      )
+      .then(
+        function(data) {
+          for (var key in data['Time Series (Daily)']) {
+            stockChartXValuesFunction.push(key);
+            stockChartYValuesFunction.push(data['Time Series (Daily)'][key]['1. open']);
+          }
+          pointerToThis.setState({
+            stockChartXValues: stockChartXValuesFunction,
+            stockChartYValues: stockChartYValuesFunction
+          });
+        }
+      )
+  }
+
+  render() {
+    
+    return (
+      <div>
+        <h2>Stock Market Price ( in USD )
+           <select  value={this.state.symbol} onChange={(event)=>
+           { this.setState({ symbol: event.target.value})
+           this.fetchStock()
+           return;
+          }
+          }  style={{margin:20,padding:10}}>
+          <option value='FB'>Facebook</option>
+          <option value='IBM'>IBM</option>
+          <option value='TSLA'>Tesla</option>
+          <option value='GOOGL'>Google</option>
+          <option value='AMZN'>Amazon</option>
+        </select></h2>
+       
+        <Plot
+          data={[
+            {
+              x: this.state.stockChartXValues,
+              y: this.state.stockChartYValues,
+              type: 'scatter',
+              mode: 'lines+markers',
+              marker: {color: 'red'},
+            }
+          ]}
+          layout={{width: 1200, height: 550, title: 'A Fancy Plot  '}}
+        />
+      </div>
+    )
+  }
+}
+
+export default Stock;
